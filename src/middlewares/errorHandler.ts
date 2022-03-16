bf5d36588deb0d459420a4a9cd7857e7f9fe0b63
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-16 16:52:31 -0300

    feat: error handler middleware

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+
+const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  if (process.env.NODE_ENV === 'production') {
+    res.json({
+      error: err.message
+    });
+  } else {
+    res.json({
+      error: err.message,
+      stack: err.stack
+    });
+  }
+};
+
+export default errorHandler;
