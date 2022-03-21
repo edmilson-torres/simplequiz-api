1f0cc5ef761ef49ac37c3b3917efc6ff80453237
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-21 11:37:12 -0300

    refactor: quiz list route

--- src/routes/quiz.ts
+++ src/routes/quiz.ts
@@ -6,8 +6,8 @@ import QuizController from '../controllers/quiz';
 const router = Router();
 const quizController = new QuizController();
 
-router.post('/quiz/', validateToken, quizController.createQuiz);
-router.get('/quizzes', validateToken, quizController.findQuizList);
+router.post('/quiz', validateToken, quizController.createQuiz);
+router.get('/quiz', validateToken, quizController.findQuizList);
 router.get('/quiz/:id', validateToken, quizController.findQuiz);
 router.delete('/quiz/:id', validateToken, quizController.deletequiz);
 
