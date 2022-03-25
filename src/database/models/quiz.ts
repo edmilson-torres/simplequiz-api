0b974f61f78cb0b496150e223189146ec7bfdbcb
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-25 10:50:57 -0300

    feat: models with validator

--- src/database/models/quiz.ts
+++ src/database/models/quiz.ts
@@ -1,27 +1,38 @@
 import { prop, getModelForClass } from '@typegoose/typegoose';
 
 class Question {
-  @prop({ required: true })
+  @prop({ required: true, minlength: 2, maxlength: 250 })
   public question: string;
 
-  @prop({ required: true })
+  @prop({ required: true, minlength: 2, maxlength: 250 })
   public answer: string;
 
-  @prop({ required: true })
-  public options: Array<string>;
+  @prop({
+    required: true,
+    validate: [(val: string[]) => val.length === 3, 'need min 3 options']
+  })
+  public options: string[];
 }
 
 export class Quiz {
-  @prop({ required: true })
+  @prop({ required: true, minlength: 2, maxlength: 250 })
   public name: string;
 
-  @prop({ required: true })
+  @prop({
+    required: true,
+    minlength: 2,
+    maxlength: 250
+  })
   public description: string;
 
-  @prop({ required: true })
+  @prop({
+    required: true,
+    minlength: 2,
+    maxlength: 80
+  })
   public category: string;
 
-  @prop({ required: true })
+  @prop({ required: true, min: 2 })
   public length: number;
 
   @prop({ required: true, type: () => [Question] })
--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,19 +1,34 @@
 import { prop, getModelForClass } from '@typegoose/typegoose';
 
 export class User {
-  @prop({ required: true })
+  @prop({
+    required: true,
+    minlength: [2, 'too short'],
+    maxlength: [250, 'too long'],
+    trim: true
+  })
   name: string;
 
-  @prop({ required: true, lowercase: true, unique: true })
+  @prop({
+    required: true,
+    lowercase: true,
+    unique: true,
+    trim: true
+  })
   email: string;
 
   @prop({ default: 'user' })
   role: string;
 
-  @prop({ required: true })
+  @prop({
+    required: true,
+    trim: true
+  })
   password: string;
 
-  @prop({ default: new Date().toISOString() })
+  @prop({
+    default: new Date().toISOString()
+  })
   createAt: Date;
 }
 const UserModel = getModelForClass(User);
