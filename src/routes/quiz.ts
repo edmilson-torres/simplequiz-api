2e89db06cdd6e95f68a12b933150153c80310853
Edmilson Torres <edmilson.torres@gmail.com>
2022-03-27 21:08:45 -0300

    feat: rolecheck

--- src/routes/quiz.ts
+++ src/routes/quiz.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import validateToken from '../middlewares/validateToken';
 import QuizController from '../controllers/quiz';
-import isAdmin from '../middlewares/isAdmin';
+import roleCheck from '../middlewares/roleCheck';
 
 const router = Router();
 const quizController = new QuizController();
@@ -10,6 +10,11 @@ const quizController = new QuizController();
 router.post('/quiz', validateToken, quizController.createQuiz);
 router.get('/quiz', validateToken, quizController.findQuizList);
 router.get('/quiz/:id', validateToken, quizController.findQuiz);
-router.delete('/quiz/:id', validateToken, isAdmin, quizController.deletequiz);
+router.delete(
+  '/quiz/:id',
+  validateToken,
+  roleCheck(['admin']),
+  quizController.deletequiz
+);
 
 export default router;
