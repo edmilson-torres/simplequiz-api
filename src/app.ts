5455b3b74399d96984132a7459e3249245d52942
Edmilson Torres <edmilson.torres@gmail.com>
2022-06-11 10:48:24 -0300

    feat: add server graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,9 @@ import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 import errorHandler from './middlewares/errorHandler';
 
+import mongoose from 'mongoose';
+import server from './server';
+
 const app = express();
 
 const database = new MongoConnection();
@@ -25,5 +28,36 @@ app.use('/api', routes);
 app.use(morgan('dev'));
 
 app.use(errorHandler);
+
+function gracefulShutdown(code: any) {
+    return (event: any) => {
+        console.info(`${event} signal received with code ${event}`);
+        console.log('Closing http server...');
+        server.close(async () => {
+            console.log('Http server closed.');
+
+            console.log('Closing DB connection...');
+            await mongoose.connection.close();
+            console.log('Mongo connection closed.');
+            process.exit(0);
+        });
+    };
+}
+
+process.on('SIGINT', gracefulShutdown('SIGINT'));
+
+process.on('SIGTERM', gracefulShutdown('SIGTERM'));
+
+process.on('exit', (code) => {
+    console.info('exit signal received.', code);
+});
+
+process.on('uncaughtException', (error, origin) => {
+    console.info(`\n${origin} signal received.`, error);
+});
+
+process.on('unhandledRejection', (error, origin) => {
+    console.info('\nunhandledRejection signal received.', error);
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 import app from './app';
 import env from './config/env';
 
-app.listen(env.port, () =>
-  console.log(`Server running at http://localhost:${env.port}`)
+const server = app.listen(env.port, () =>
+    console.log(`Server running at http://localhost:${env.port}`)
 );
+
+export default server;
