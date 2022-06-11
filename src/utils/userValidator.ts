ea70010ffdb0f65791bccc4fd7a73fefe2850c3f
Edmilson Torres <edmilson.torres@gmail.com>
2022-06-11 10:51:35 -0300

    refactor: change user validator file name

---
+++ src/utils/userValidator.ts
@@ -0,0 +1,29 @@
+import * as Yup from 'yup';
+
+const schema = Yup.object().shape({
+    email: Yup.string()
+        .email('Must be a valid email')
+        .required('Email is required'),
+    name: Yup.string()
+        .min(2, 'Name must be longer than 2 characters')
+        .max(255, 'Name must be less than 255 characters long')
+        .required('name is required'),
+    password: Yup.string()
+        .min(6, 'Password must be longer than 6 characters')
+        .max(255, 'Password must be less than 255 characters long')
+        .required('Password is required'),
+    role: Yup.mixed().oneOf(['admin', 'user'])
+});
+
+async function registerValidator(form: Object): Promise<boolean | string> {
+    const result = await schema.isValid(form);
+    const error = await schema.validate(form).catch((err) => err.errors);
+
+    if (!result) {
+        return error.toString();
+    }
+
+    return result;
+}
+
+export default registerValidator;
