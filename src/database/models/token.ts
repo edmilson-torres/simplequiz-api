069c958b4a661625cb667fbee254ecf2027ce491
Edmilson Torres <edmilson.torres@gmail.com>
2022-06-16 18:26:13 -0300

    feat: token model

---
+++ src/database/models/token.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+import { prop, getModelForClass } from '@typegoose/typegoose';
+
+export class Token {
+    @prop({
+        required: true,
+        ref: 'user'
+    })
+    userId: mongoose.Types.ObjectId;
+
+    @prop({
+        required: true
+    })
+    token: string;
+
+    @prop({
+        required: true,
+        default: Date.now,
+        expires: 900
+    })
+    createAt: Date;
+
+    @prop()
+    updateAt: Date;
+}
+const TokenModel = getModelForClass(Token);
+
+export default TokenModel;
