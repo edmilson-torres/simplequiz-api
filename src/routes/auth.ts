56575f2021fcda7704f4dfdfd34b26e9de7d7749
Edmilson Torres <edmilson.torres@gmail.com>
2022-06-16 18:29:09 -0300

    feat: add reset password routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
-import UserController from '../controllers/user';
 import { Router } from 'express';
+import UserController from '../controllers/user';
 import AuthController from '../controllers/auth';
 
 const router = Router();
@@ -8,5 +8,7 @@ const authController = new AuthController();
 
 router.post('/auth/register', userController.createUser);
 router.post('/auth/login', authController.login);
+router.post('/auth/requestResetPassword', authController.resetPasswordRequest);
+router.post('/auth/resetPassword', authController.resetPassword);
 
 export default router;
