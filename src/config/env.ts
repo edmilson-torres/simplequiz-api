f58f16ab45223c3321de80b59fb34ed0dff4d7ce
Edmilson Torres <edmilson.torres@gmail.com>
2022-06-19 17:40:58 -0300

    chore(env): add email credentials

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,11 @@
 export default {
     port: process.env.PORT || 5000,
     mongoUrl: process.env.MONGO_URL,
-    secretJWT: process.env.SECRET_JWT
+    secretJWT: process.env.SECRET_JWT,
+    clientUrl: process.env.CLIENT_URL,
+    emailHost: process.env.EMAIL_HOST,
+    emailUserName: process.env.EMAIL_USERNAME,
+    emailPort: process.env.EMAIL_PORT,
+    emailPassword: process.env.EMAIL_PASSWORD,
+    fromEmail: process.env.FROM_EMAIL
 };
