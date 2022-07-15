841a117c491019872f42cc8c21c9989104b2b1ad
Edmilson Torres <edmilson.torres@gmail.com>
2022-07-14 22:55:48 -0300

    chore: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
-  }
-}
+    roots: ['<rootDir>/src'],
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1'
+    }
+};
