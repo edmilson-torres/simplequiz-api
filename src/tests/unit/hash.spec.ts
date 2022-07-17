ca4987472a5f5974c6a21be35c1b3a36f0dcf023
Edmilson Torres <edmilson.torres@gmail.com>
2022-07-16 23:44:50 -0300

    test(unit): add hash and jwt

---
+++ src/tests/unit/hash.spec.ts
@@ -0,0 +1,15 @@
+import { compareStringHash, createStringHash } from '@/utils/hash';
+
+describe('Hash', () => {
+    it('should create hash', async () => {
+        expect(await createStringHash('123456')).toMatch(/^\$2[ayb]\$.{56}$/gm);
+    });
+    it('should compare hash return true', async () => {
+        const hash = await createStringHash('123456');
+        expect(await compareStringHash('123456', hash)).toBe(true);
+    });
+    it('should compare hash return false', async () => {
+        const hash = await createStringHash('abcdef');
+        expect(await compareStringHash('123456', hash)).toBe(false);
+    });
+});
---
+++ src/tests/unit/jwt.spec.ts
@@ -0,0 +1,20 @@
+import { signJwt, verifyJwt } from '@/utils/jwt';
+
+const payload = {
+    sub: '507f1f77bcf86cd799439011',
+    role: 'user'
+};
+const token = signJwt(payload);
+
+describe('JWT', () => {
+    it('should create a jwt', () => {
+        expect(token).toMatch(
+            /^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-\+\/=]*)/gi
+        );
+    });
+    it('should verify a jwt', () => {
+        const decoded = verifyJwt(token);
+        expect(decoded).toHaveProperty('role', 'user');
+        expect(decoded).toHaveProperty('sub', '507f1f77bcf86cd799439011');
+    });
+});
