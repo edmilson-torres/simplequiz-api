93eea64e9a3597b38d5a03976c3b58012c9aa5c0
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-10 15:35:47 -0300

    refactor: remove unnecessary await

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,9 +1,8 @@
-import UserModel from '../database/models/user';
-import User from '../entities/user';
+import UserModel, { User } from '../database/models/user';
 
 class UserRepository {
     public async findUserById(id: string): Promise<User> {
-        return await UserModel.findById(id, [
+        return UserModel.findById(id, [
             '-createAt',
             '-password',
             '-__v'
@@ -11,15 +10,15 @@ class UserRepository {
     }
 
     public async findUserByEmail(email: string): Promise<User> {
-        return await UserModel.findOne({ email: email }).lean();
+        return UserModel.findOne({ email: email }).lean();
     }
 
     public async findUserList(): Promise<Array<User>> {
-        return await UserModel.find({}, '-password').lean();
+        return UserModel.find({}, '-password').lean();
     }
 
     public async createUser(user: Object) {
-        return await UserModel.create(user);
+        return UserModel.create(user);
     }
 
     public async updateUser(
@@ -27,7 +26,7 @@ class UserRepository {
         name?: string,
         role?: string
     ): Promise<User> {
-        return await UserModel.findByIdAndUpdate(
+        return UserModel.findByIdAndUpdate(
             { _id: id },
             { name: name, role: role },
             { fields: { password: 0, __v: 0, createAt: 0 }, new: true }
@@ -43,7 +42,7 @@ class UserRepository {
     }
 
     public async deleteUser(id: string): Promise<Object> {
-        return await UserModel.deleteOne({ _id: id });
+        return UserModel.deleteOne({ _id: id });
     }
 }
 
