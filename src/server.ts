92fdfd222dd4ef330336a4300bd0ed8f9f77c5e1
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-12 23:26:18 -0300

    refactor: run server only in test env

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,11 @@ import 'dotenv/config';
 import app from './app';
 import env from './config/env';
 
-const server = app.listen(env.port, () =>
-    console.log(`Server running at http://localhost:${env.port}`)
-);
+let server;
+if (process.env.NODE_ENV !== 'test') {
+    server = app.listen(env.port, () =>
+        console.log(`Server running at http://localhost:${env.port}`)
+    );
+}
 
 export default server;
