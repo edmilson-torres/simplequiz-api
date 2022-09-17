29da39b59e534a25ae8a5a50522279f6a7c8ad1e
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-17 19:31:33 -0300

    chore: add _id in mock users

--- tests/mock/users.ts
+++ tests/mock/users.ts
@@ -1,11 +1,13 @@
 export const users = [
     {
+        _id: '632616d05b383a5cf6200420',
         name: 'admin',
         email: 'admin@mail.com',
         role: 'admin',
         password: '$2b$12$by37jRV6pClf1.aftCqxqONPaJWpqwAeunJk/E2l3jVsz4kJ4LcAe'
     },
     {
+        _id: '632616df38b680c9ad0d4c88',
         name: 'test',
         email: 'test@mail.com',
         role: 'user',
