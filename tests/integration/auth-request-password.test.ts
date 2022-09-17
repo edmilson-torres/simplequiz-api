3ea4d8f9973ce10f2252eae43c8f7d897d2ab0ad
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-17 19:32:05 -0300

    test: add invalid e-mail test

--- tests/integration/auth-request-password.test.ts
+++ tests/integration/auth-request-password.test.ts
@@ -20,23 +20,29 @@ describe('Integration Auth request reset password', () => {
         await mongoose.disconnect();
     });
 
-    it('should return a reset link', async () => {
+    it('should return 200 with a reset link', async () => {
         const spy = jest
             .spyOn(sendTestEmail, 'sendTestEmail')
-            .mockResolvedValue('string');
+            .mockResolvedValue('tokenMock');
         const res = await request(app)
             .post('/api/auth/requestresetpassword')
             .send({ email: 'test@mail.com' });
 
         expect(res.statusCode).toBe(200);
         expect(res.body).toHaveProperty('link');
-        expect(res.body.link).toBe('string');
+        expect(res.body.link).toBe('tokenMock');
         expect(spy).toBeCalledTimes(1);
     });
-    it('should return not e-mail registered ', async () => {
+    it('should return 404 e-mail not registered ', async () => {
         const res = await request(app)
             .post('/api/auth/requestresetpassword')
-            .send({ email: 'nor.registered.email@mail.com' });
+            .send({ email: 'not.registered.email@mail.com' });
         expect(res.statusCode).toBe(404);
+    });
+    it('should return 400 invalid e-mail', async () => {
+        const res = await request(app)
+            .post('/api/auth/requestresetpassword')
+            .send({ email: 'invalidmail.com' });
+        expect(res.statusCode).toBe(400);
     });
 });
