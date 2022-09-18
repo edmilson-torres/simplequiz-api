cb06ee87cfae81976ca747e5ed8be24e5075f32c
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-17 21:42:55 -0300

    chore: remove unnecessary  prop dont need by shelf/jest-mongodb

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
     collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
     coverageDirectory: 'coverage',
     coverageProvider: 'babel',
-    testEnvironment: 'node',
     transform: {
         '.+\\.ts$': 'ts-jest'
     },
