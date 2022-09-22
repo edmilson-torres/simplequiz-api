9f503f3c0fdea2cc3cdcb9e1a879c1b3a44e6037
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-21 23:36:12 -0300

    test: add find user by id

--- tests/integration/user-find-by-id.test.ts
+++ tests/integration/user-find-by-id.test.ts
@@ -14,12 +14,34 @@ describe('Integration User find by id', () => {
         await mongoose.disconnect();
     });
     afterEach(() => jest.clearAllMocks());
-    it.skip('should find user by id', async () => {
-        const res = await request(app).get('/api/users').send();
+
+    it('should find users by id using admin role', async () => {
+        const login = await request(app).post('/api/auth/login').send({
+            email: 'admin@mail.com',
+            password: '123456'
+        });
+        const res = await request(app)
+            .get('/api/users')
+            .set('Authorization', `Bearer ${login.body.user.token}`);
         expect(res.statusCode).toBe(200);
+        expect(Array.isArray(res.body)).toBe(true);
     });
-    it('should return 401 without admin role', async () => {
+
+    it('should return 401 without token', async () => {
         const res = await request(app).get('/api/users').send();
         expect(res.statusCode).toBe(401);
+        expect(res.body.error).toBe('token is missing');
+    });
+
+    it('should return 403 without admin role', async () => {
+        const login = await request(app).post('/api/auth/login').send({
+            email: 'test@mail.com',
+            password: '123456'
+        });
+        const res = await request(app)
+            .get('/api/users')
+            .set('Authorization', `Bearer ${login.body.user.token}`);
+        expect(res.statusCode).toBe(403);
+        expect(res.body.error).toBe('unauthorized');
     });
 });
