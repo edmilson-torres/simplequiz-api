1139873262a49d37183daa6b3bea121a266d646d
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-30 23:39:16 -0300

    refactor: add login token mock

--- tests/integration/user-list.test.ts
+++ tests/integration/user-list.test.ts
@@ -6,9 +6,26 @@ import mongoose from 'mongoose';
 import UserModel from '../../src/database/models/user';
 import { users } from '../mock/users';
 
+let userAccessToken: string;
+let adminAccessToken: string;
+
 describe('Integration User list', () => {
     beforeAll(async () => {
         await UserModel.insertMany(users);
+        const userSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'test@mail.com',
+                password: '123456'
+            });
+        userAccessToken = userSignInResponse.body.user.token;
+        const adminSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'admin@mail.com',
+                password: '123456'
+            });
+        adminAccessToken = adminSignInResponse.body.user.token;
     });
     afterAll(async () => {
         await UserModel.deleteMany();
@@ -22,24 +39,16 @@ describe('Integration User list', () => {
     });
 
     it('should return forbidden, without admin role', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'test@mail.com',
-            password: '123456'
-        });
         const res = await request(app)
             .get('/api/users')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
+            .set('Authorization', `Bearer ${userAccessToken}`);
         expect(res.statusCode).toBe(httpCode.FORBIDDEN);
     });
 
     it('should list all users', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'admin@mail.com',
-            password: '123456'
-        });
         const res = await request(app)
             .get('/api/users')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
+            .set('Authorization', `Bearer ${adminAccessToken}`);
         expect(res.statusCode).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
     });
