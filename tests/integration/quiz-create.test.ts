4b58371508565d41f904af254a3b22b62d595037
Edmilson Torres <edmilson.torres@gmail.com>
2022-10-01 11:23:01 -0300

    test: add quiz create tests

--- tests/integration/quiz-create.test.ts
+++ tests/integration/quiz-create.test.ts
@@ -9,8 +9,25 @@ import { quizzies } from '../mock/quizzies';
 import { httpCode } from '../../src/utils/httpCode';
 
 let userAccessToken: string;
-let adminAccessToken: string;
 
+const quizMock = {
+    category: 'new category',
+    description: 'example',
+    name: 'quiz example',
+    createAt: '2022-03-21T01:31:42.020+00:00',
+    questions: [
+        {
+            question: 'how to example 1?',
+            answer: 'right answer',
+            options: ['wrong  answer 1', 'wrong  answer 2', 'wrong  answer 3']
+        },
+        {
+            question: 'how to example 2?',
+            answer: 'right answer',
+            options: ['wrong  answer 1', 'wrong  answer 2', 'wrong  answer 3']
+        }
+    ]
+};
 describe('Integration Quiz create', () => {
     beforeAll(async () => {
         await UserModel.insertMany(users);
@@ -22,14 +39,8 @@ describe('Integration Quiz create', () => {
                 password: '123456'
             });
         userAccessToken = userSignInResponse.body.user.token;
-        const adminSignInResponse = await request(app)
-            .post('/api/auth/login')
-            .send({
-                email: 'admin@mail.com',
-                password: '123456'
-            });
-        adminAccessToken = adminSignInResponse.body.user.token;
     });
+
     afterAll(async () => {
         await QuizModel.deleteMany();
         await UserModel.deleteMany();
@@ -41,14 +52,12 @@ describe('Integration Quiz create', () => {
         expect(res.statusCode).toBe(httpCode.UNAUTHORIZED);
     });
 
-    it('should return all quizzies', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'test@mail.com',
-            password: '123456'
-        });
+    it('should return Ok to create a new quiz', async () => {
         const res = await request(app)
-            .get('/api/quiz')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
-        expect(res.statusCode).toBe(httpCode.OK);
+            .post('/api/quiz')
+            .set('Authorization', `Bearer ${userAccessToken}`)
+            .send(quizMock);
+        expect(res.statusCode).toBe(httpCode.CREATED);
+        expect(res.body.quiz.name).toBe(quizMock.name);
     });
 });
