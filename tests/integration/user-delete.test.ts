2683a10f66f63220a2806b19e98596393dd90fa5
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-30 23:36:03 -0300

    refactor: use login in beforeAll hook

--- tests/integration/user-delete.test.ts
+++ tests/integration/user-delete.test.ts
@@ -6,9 +6,26 @@ import mongoose from 'mongoose';
 import UserModel from '../../src/database/models/user';
 import { users } from '../mock/users';
 
+let userAccessToken: string;
+let adminAccessToken: string;
+
 describe('Integration User delete', () => {
     beforeAll(async () => {
         await UserModel.insertMany(users);
+        const userSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'test@mail.com',
+                password: '123456'
+            });
+        userAccessToken = userSignInResponse.body.user.token;
+        const adminSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'admin@mail.com',
+                password: '123456'
+            });
+        adminAccessToken = adminSignInResponse.body.user.token;
     });
     afterAll(async () => {
         await UserModel.deleteMany();
@@ -24,34 +41,22 @@ describe('Integration User delete', () => {
     });
 
     it('should return forbidden, without admin role', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'test@mail.com',
-            password: '123456'
-        });
         const res = await request(app)
             .delete('/api/users/632616df38b680c9ad0d4c88')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
+            .set('Authorization', `Bearer ${userAccessToken}`);
         expect(res.statusCode).toBe(httpCode.FORBIDDEN);
     });
 
     it('should return user not found', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'admin@mail.com',
-            password: '123456'
-        });
         const res = await request(app)
             .delete('/api/users/632fb514e7e90e4a4699a298')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
+            .set('Authorization', `Bearer ${adminAccessToken}`);
         expect(res.statusCode).toBe(httpCode.NOT_FOUND);
     });
     it('should delete user by id', async () => {
-        const login = await request(app).post('/api/auth/login').send({
-            email: 'admin@mail.com',
-            password: '123456'
-        });
         const res = await request(app)
             .delete('/api/users/632616df38b680c9ad0d4c88')
-            .set('Authorization', `Bearer ${login.body.user.token}`);
+            .set('Authorization', `Bearer ${adminAccessToken}`);
         expect(res.statusCode).toBe(204);
     });
 });
