b38b9d1628c2af049d0e77c45ebe14999cb44fb4
Edmilson Torres <edmilson.torres@gmail.com>
2022-09-30 23:34:05 -0300

    test: add quiz create tests

--- tests/integration/quiz-create.test.ts
+++ tests/integration/quiz-create.test.ts
@@ -1,3 +1,54 @@
+import request from 'supertest';
+import app from '../../src/app';
+
+import mongoose from 'mongoose';
+import UserModel from '../../src/database/models/user';
+import QuizModel from '../../src/database/models/quiz';
+import { users } from '../mock/users';
+import { quizzies } from '../mock/quizzies';
+import { httpCode } from '../../src/utils/httpCode';
+
+let userAccessToken: string;
+let adminAccessToken: string;
+
 describe('Integration Quiz create', () => {
-    it.todo('should create quiz by id');
+    beforeAll(async () => {
+        await UserModel.insertMany(users);
+        await QuizModel.insertMany(quizzies);
+        const userSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'test@mail.com',
+                password: '123456'
+            });
+        userAccessToken = userSignInResponse.body.user.token;
+        const adminSignInResponse = await request(app)
+            .post('/api/auth/login')
+            .send({
+                email: 'admin@mail.com',
+                password: '123456'
+            });
+        adminAccessToken = adminSignInResponse.body.user.token;
+    });
+    afterAll(async () => {
+        await QuizModel.deleteMany();
+        await UserModel.deleteMany();
+        await mongoose.disconnect();
+    });
+
+    it('should return unauthorized, without token', async () => {
+        const res = await request(app).post('/api/quiz');
+        expect(res.statusCode).toBe(httpCode.UNAUTHORIZED);
+    });
+
+    it('should return all quizzies', async () => {
+        const login = await request(app).post('/api/auth/login').send({
+            email: 'test@mail.com',
+            password: '123456'
+        });
+        const res = await request(app)
+            .get('/api/quiz')
+            .set('Authorization', `Bearer ${login.body.user.token}`);
+        expect(res.statusCode).toBe(httpCode.OK);
+    });
 });
